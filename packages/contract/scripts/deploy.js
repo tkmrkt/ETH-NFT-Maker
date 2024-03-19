5f5a17f56c93e86c7ac64bb61b3393944c37ca6c
tkmrkt <tkmrkt@gmail.com>
2024-03-19 15:59:22 +0900

    fix

--- packages/contract/scripts/deploy.js
+++ packages/contract/scripts/deploy.js
@@ -1,32 +1,33 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const main = async () => {
+  // コントラクトがコンパイルします
+  // コントラクトを扱うために必要なファイルが `artifacts` ディレクトリの直下に生成されます。
+  const nftContractFactory = await hre.ethers.getContractFactory('Web3Mint');
+  // Hardhat がローカルの Ethereum ネットワークを作成します。
+  const nftContract = await nftContractFactory.deploy();
+  // コントラクトが Mint され、ローカルのブロックチェーンにデプロイされるまで待ちます。
+  await nftContract.deployed();
+  console.log('Contract deployed to:', nftContract.address);
+  // makeAnEpicNFT 関数を呼び出す。NFT が Mint される。
+  let txn = await nftContract.makeAnEpicNFT();
+  // Minting が仮想マイナーにより、承認されるのを待ちます。
+  await txn.wait();
+  console.log('Minted NFT #1');
+  // makeAnEpicNFT 関数をもう一度呼び出します。NFT がまた Mint されます。
+  txn = await nftContract.makeAnEpicNFT();
+  // Minting が仮想マイナーにより、承認されるのを待ちます。
+  await txn.wait();
+  console.log('Minted NFT #2');
+};
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+// エラー処理を行っています。
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+runMain();
